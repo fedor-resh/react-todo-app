2e5216ad7e551b2ca3468a1a2cb2c3812ec1f302
fedor-resh <87930362+fedor-resh@users.noreply.github.com>
2022-03-01 17:26:45 +0300

    ///

--- src/SettingsPanel/SettingsPanel.js
+++ src/SettingsPanel/SettingsPanel.js
@@ -4,13 +4,21 @@ import './SettingsPanel.module.css'
 import Slider from '../PomodoroPanel/Slider/Slider';
 const SettingsPanel = (props) => {
     const [seconds,setSeconds] = useState(0)
-    useEffect(()=>props.toSetTime(seconds/60),[Math.round(seconds/60)])
+    useEffect(()=>{
+        props.toSetTime(seconds/60)
+    },[Math.round(seconds/60)])
+    useEffect(()=> setSeconds(Math.round(props.task?props.task.timeToDo*60:0)),
+        [props.selectedId])
     return (
         <div >
             <div className={styles.panel+' '+
                 (props.selectedId!==undefined?styles.NotFade:styles.fade)}>
+                <button onClick={()=>props.setSelectedId(undefined)}
+                        className={styles.close__btn}>
+                    close
+                </button>
                 <div style={{width:'min(300px,100%)',float:'right'}}>
-                    <h1>Settings</h1>
+                    <h1 >Settings</h1>
                     <input
                         value={props.task?props.task.text:''}
                         onChange={(el)=>
@@ -24,8 +32,8 @@ const SettingsPanel = (props) => {
                     />
                     <div style={{height:20}}/>
                         <Slider
-                            valueOfSlider={seconds}
-                            setValueOfSlider={(x)=>setSeconds(x)}
+                            value={seconds}
+                            setValue={(x)=>setSeconds(x)}
                             timeToDo={props.task?props.task.timeToDo:0}
                             selectedId={props.selectedId}
                             isTimePanelVisible={true}
